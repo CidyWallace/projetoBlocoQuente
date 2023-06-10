ada15e4f7705654b1a571b4bc5e2f3f431ab2872
JonathanNevesRibeiro <jonathan.neves@dcx.ufpb.br>
2023-06-10 11:38:31 -0300

    testando

---
+++ src/main/java/org/example/classes/Funcionario.java
@@ -0,0 +1,8 @@
+package org.example.classes;
+
+import java.util.ArrayList;
+
+public class Funcionario {
+    private String nomes;
+    private String senha = "123";
+}
---
+++ src/main/java/org/example/classes/Mesa.java
@@ -0,0 +1,11 @@
+package org.example.classes;
+
+public class Mesa {
+    private int numero;
+    private boolean disponivel;
+
+    public Mesa(int numero, boolean disponivel) {
+        this.numero = numero;
+        this.disponivel = disponivel;
+    }
+}
