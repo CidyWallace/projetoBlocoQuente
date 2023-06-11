ec1acc31ba04e98a9c9c3721dcb997625c6d5f23
JonathanNevesRibeiro <jonathan.neves@dcx.ufpb.br>
2023-06-11 11:47:01 -0300

    alteração em diversas clases (funcionario cardapio pedidoRep e main)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,38 +1,84 @@
 package org.example;
 
-import org.example.classes.Cardapio;
-import org.example.classes.Mesa;
-import org.example.classes.Pedido;
-import org.example.classes.PedidoRep;
+import org.example.classes.*;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Cardapio cardapio = new Cardapio();
+        cardapio.add_nomes();
+        cardapio.add_precos();
         PedidoRep repositorio = new PedidoRep();
+        Funcionario funcionario = new Funcionario();
 
         for (int i = 0; i < 10; i++) {
             Mesa mesa = new Mesa(i, true);
             repositorio.addMesa(mesa);
         }
+        boolean sair = false;
+        while (!sair){
+            Scanner in = new Scanner(System.in);
+            System.out.println();
+            System.out.println("\nBem vindo ao restaurante Bloco Quente");
+            System.out.println("Digite oque desaja realizar");
+            System.out.println("0.Cardapio\n1.Realizar pedido\n2.Menu para funcionarios");
+            int opcao = in.nextInt();
+            switch (opcao) {
+                case (0) -> {
+                    System.out.println(cardapio.todosOsItens());
+                }
+                case (1) -> {
+                    System.out.println("Para realizar o pedido coloque o nome do item do menu");
+                    in.nextLine();
+                    String escolha = in.nextLine();
+                    Pedido pedido = new Pedido(escolha, cardapio);
+                    repositorio.addPedido(pedido);
+                }
+                case (2) -> {
+                    System.out.println("Para acessar o menu do funcionario informe a senha:");
+                    in.nextLine();
+                    String senha = in.nextLine();
+                    if(funcionario.validaSenha(senha)){
+                        boolean sairMenuFuncionario = false;
+                        while(!sairMenuFuncionario){
+                        System.out.println("Menu do funcionario");
+                        System.out.println("Digite oque deseja realizar:");
+                        System.out.println("0. Alterar pedido\n1. Concluir pedido do cliente\n2. Gerar relatorio e salvar\n3. Sair do menu funcionario\n");
+                        opcao = in.nextInt();
+                        int escolha;
+                        String escolha1;
+                        switch (opcao){
+                            case (0) -> {
+                                System.out.println(repositorio.getRelatorios());
+                                System.out.println("Digite o numero do pedido que deseja alterar: \n");
+                                escolha = in.nextInt();
+                                System.out.println(cardapio.todosOsItens());
+                                System.out.println("Digite para oque deseja alterar: \n");
+                                in.nextLine();
+                                escolha1 = in.nextLine();
+                                repositorio.mudarPedido(escolha, escolha1);
 
-        System.out.println( repositorio.pegarMesa(1));
+                            }
+                            case (1) -> {
+                                System.out.println(repositorio.getRelatorios());
+                                System.out.println("\nDigite o numero do pedido que deseja concluir: \n");
+                                escolha = in.nextInt(); in.nextLine();
+                                repositorio.pagarPedido(escolha);
 
-        cardapio.add_nomes();
-        cardapio.add_precos();
+                            }
+                            case (2) -> {
+                                System.out.println(repositorio.getRelatorios());
+                                System.out.println("\nRepresentação do relatorio salvo");
+                            }
+                            case (3) -> {
+                                sairMenuFuncionario = true;
+                            }
+                        }
+                            }
+                        }
 
-        Pedido pedido= new Pedido("Sanduíche Fumegante", cardapio);
-        Pedido pedido1= new Pedido("Pizza em Chamas", cardapio);
-        Pedido pedido2= new Pedido("Suco Frutas em Fogo", cardapio);
-
-        repositorio.addPedido(pedido);
-        repositorio.addPedido(pedido1);
-        repositorio.addPedido(pedido2);
-
-        System.out.println(repositorio.pegaPedido(0));
-
-        repositorio.mudarPedido(0, "Suco Frutas em Fogo");
-
-        System.out.println(repositorio.pegaPedido(0));
-        //repositorio.getRelatorios();
-    }
-}
+                    }
+                }
+            }
+    }}
